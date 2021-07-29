39e43140fb9c247ca0f9f06977b9bc955dfe28e6
TokaAbdelhakim <lulaiola32@gmail.com>
2021-07-29 20:17:34 +0200

    PY_9

---
+++ 9_circle.py
@@ -0,0 +1,8 @@
+import math
+radius = float (input("Enter circle radius: "))
+pi = 3.14
+circumference = 2 * pi * radius
+area = pi * (radius ** 2)
+
+print(" Circumference = %.1f" %circumference)
+print(" Area = %.1f" %area)
