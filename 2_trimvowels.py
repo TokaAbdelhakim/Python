7e7a162d6ed2994d87c6d7f2b7d75bf1b4fd5b66
TokaAbdelhakim <lulaiola32@gmail.com>
2021-07-29 20:14:47 +0200

    PY_2

---
+++ 2_trimvowels.py
@@ -0,0 +1,13 @@
+import sys 
+str = sys.argv[1]
+str_len = len(str)
+new_str=""
+vowels = ["a","e","o","i","u"]
+for i in range(str_len):
+    if str[i] not in vowels:
+        new_str=new_str+str[i]
+print (new_str)
+
+
+#new_str = (l for l in str if l not in vowels)
+#new_str = ''.join(new_str)
