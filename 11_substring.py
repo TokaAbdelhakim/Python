3fd2ca9c7f15a8af7cd1ad0aae5dd842c5723e3d
TokaAbdelhakim <lulaiola32@gmail.com>
2021-07-30 18:49:01 +0200

    PY_11

--- 11_substring.py
+++ 11_substring.py
@@ -5,14 +5,22 @@ def subString (user_string):
     longest_string = user_string[0]
 
     for c in  user_string[1:]:
-        if c >= current_string [-1]:
-            current_string = current_string + c 
-            print (current_string)
-            if len(current_string)>= len(longest_string) :
-                longest_string = current_string
+        if c >= current_string[-1]:
+            current_string += c 
+
+            if len(current_string)> len(longest_string) :
+                longest_string = current_string               
         else:
             current_string = c
-        return longest_string
 
-print (subString(user_string))
+        if len(current_string) == len(longest_string) :
+             i = 0            
+             for l in longest_string[0:]:
+                if l >= current_string[i]:
+                     longest_string = current_string
+                     i+=1
+            
+    return longest_string
+
+print ('Substring: ' + subString(user_string))
 
